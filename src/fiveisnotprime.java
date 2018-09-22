e1fd236b1ae39fa077c30db9c132c499594620fd
Jack <league@WorthyMini-5.attlocal.net>
2018-09-21 18:26:49 -0700

    finished CircleRing and prime or not

--- src/fiveisnotprime.java
+++ src/fiveisnotprime.java
@@ -1,4 +1,31 @@
+import javax.swing.JOptionPane;
 
 public class fiveisnotprime {
 
+public static void main(String[] args) {
+	String prime =JOptionPane.showInputDialog("Enter a Number And see If It's Prime");
+int hi = Integer.parseInt(prime);
+	
+for (int i = 2; i < hi; i++) {
+	
+
+
+if(hi%i==0) {JOptionPane.showMessageDialog(null,"IT'S PRIME");} else {JOptionPane.showMessageDialog(null,"IT'S NOT PRIME");}
+break;}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
 }
